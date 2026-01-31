ccfafc967980aa596d151636a4f7d79e33c02e4d
Paul Mark White <paulmarkwhite@hotmail.co.uk>
2026-01-31 20:28:40 +0000

    Added in footsteps.

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -10,19 +10,32 @@ public class PlayerController : MonoBehaviour
 
     MovementController controller;
 
+
+    AudioSource Footsteps; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Move = InputSystem.actions.FindAction("Move");
         Jump = InputSystem.actions.FindAction("Jump"); 
         controller = GetComponent<MovementController>();
+        Footsteps = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movement = Move.ReadValue<Vector2>(); 
+        Vector2 movement = Move.ReadValue<Vector2>();
         controller.Movement = movement;
+        if (movement != Vector2.zero && !Footsteps.isPlaying)
+        {
+            
+            Footsteps.Play();
+        }
+        else
+        {
+            Footsteps.Pause(); 
+        }
 
         controller.Jump = Jump.IsPressed(); 
     }
